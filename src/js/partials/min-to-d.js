4f1157c1d1ce98eadadc7f4ed161773d3edf73d4
ansachuk <ansachuk@icloud.com>
2023-03-11 00:15:26 +0200

    some refact

--- src/js/partials/min-to-d.js
+++ src/js/partials/min-to-d.js
@@ -1,11 +1,8 @@
 export const transformMinutesToDays = () => {
   const enteredMinutes = Number(prompt('How many minutes you have?'));
-  const minutes = enteredMinutes % 60;
   const fullHours = Math.floor(enteredMinutes / 60);
-  const hoursFloor = fullHours % 24;
-  const fullDays = Math.floor(fullHours / 24);
 
-  return ` You have ${fullDays} days, ${hoursFloor} hours, ${minutes} minutes.`;
+  return ` You have ${Math.floor(fullHours / 24)} days, ${
+    fullHours % 24
+  } hours, ${enteredMinutes % 60} minutes.`;
 };
-
-// console.log(transformMinutesToDays(enteredMinutes));
